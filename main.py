b3007f9c97fb12b43fc53db3a57b5eadf42ff8ff
Sofiya Dassayeva <140627941+dlotsss@users.noreply.github.com>
2025-02-15 18:56:53 +0500

    data cleaning

--- main.py
+++ main.py
@@ -14,3 +14,9 @@ print("Data frame shape:", df.shape)
 print("\nData types and non-null counts:\n", df.info())
 print("\nBasic stats:\n", df.describe())
 
+cols_with_many_nans = ["Alley", "PoolQC", "Fence", "MiscFeature"]
+df.drop(columns=cols_with_many_nans, inplace=True)
+
+num_features = df.select_dtypes(include=[np.number]).columns
+num_imputer = SimpleImputer(strategy="median")
+df[num_features] = num_imputer.fit_transform(df[num_features])
